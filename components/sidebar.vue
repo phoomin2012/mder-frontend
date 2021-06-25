a4a84f68f0ed1e9aa53f29c46a38de286379f25a
phoomin009 <phoomin009@gmail.com>
2021-06-25 23:09:39 +0700

    Add fontawesome icon

--- components/sidebar.vue
+++ components/sidebar.vue
@@ -3,33 +3,60 @@
     <ul>
       <li>
         <nuxt-link to="/dashboard">
+          <fa-icon icon="tachometer-alt" size="lg" />
           Dashboard
         </nuxt-link>
       </li>
 
       <li>
         <nuxt-link to="/patient/add">
+          <fa-icon icon="user-injured" size="lg" />
           Add patient
         </nuxt-link>
       </li>
 
       <li>
         <nuxt-link to="/staff">
+          <fa-icon icon="user-md" size="lg" />
           Staff management
         </nuxt-link>
       </li>
 
       <li v-if="false">
         <nuxt-link to="/history">
+          <fa-icon icon="chart-area" size="lg" />
           History
         </nuxt-link>
       </li>
     </ul>
+
+    <div>
+      <ul>
+        <li>
+          <a href="#logout" @click.prevent="logout">
+            <fa-icon icon="sign-out-alt" size="lg" />
+            Logout
+          </a>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-
+  methods: {
+    async logout () {
+      await this.$auth.logout()
+    }
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+  .sidebar {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
+</style>
