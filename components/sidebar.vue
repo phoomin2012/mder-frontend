adbe698b52150b7c7d8a3344cbea9dd21657d0e0
phoomin009 <phoomin009@gmail.com>
2021-06-25 22:20:05 +0700

    Make sidebar

--- components/sidebar.vue
+++ components/sidebar.vue
@@ -1,5 +1,31 @@
 <template>
-  <div />
+  <div class="sidebar">
+    <ul>
+      <li>
+        <nuxt-link to="/dashboard">
+          Dashboard
+        </nuxt-link>
+      </li>
+
+      <li>
+        <nuxt-link to="/patient/add">
+          Add patient
+        </nuxt-link>
+      </li>
+
+      <li>
+        <nuxt-link to="/staff">
+          Staff management
+        </nuxt-link>
+      </li>
+
+      <li v-if="false">
+        <nuxt-link to="/history">
+          History
+        </nuxt-link>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -15,13 +15,53 @@ export default {
 </script>
 
 <style lang="scss">
-  .app-wrapper {
-    display: flex;
-    & > div {
-      &.sidebar {
-        min-width: 130px;
-        max-width: 270px;
+html {
+  height: 100%;
+  body {
+    height: 100%;
+    div#__nuxt {
+      height: 100%;
+      div#__layout {
+        height: 100%;
       }
     }
   }
+}
+
+.app-wrapper {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  & > div {
+    // Sidebar
+    &.sidebar {
+      min-width: 270px;
+      background: #ffaa11;
+      padding: 2.5rem;
+
+      ul {
+        list-style-type: none;
+        padding: 0;
+
+        li {
+            padding-top: 10px;
+            padding-bottom: 10px;
+
+          a {
+            display: block;
+            text-decoration: none;
+          }
+        }
+      }
+    }
+
+    // Content
+    &:last-child {
+      flex-grow: 1;
+      background: #5a8921;
+      overflow: auto;
+      padding: 2.5rem;
+    }
+  }
+}
 </style>
