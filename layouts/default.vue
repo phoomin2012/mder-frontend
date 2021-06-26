29fe99af43e202224be316a3468f6cb7b063290b
phoomin009 <phoomin009@gmail.com>
2021-06-26 13:07:58 +0700

    Fix layout when show a popup

--- layouts/default.vue
+++ layouts/default.vue
@@ -15,16 +15,14 @@ export default {
 </script>
 
 <style lang="scss">
-html {
+
+div#__nuxt {
+  position: absolute;
+  width: 100%;
   height: 100%;
-  body {
-    height: 100%;
-    div#__nuxt {
-      height: 100%;
-      div#__layout {
-        height: 100%;
-      }
-    }
+
+  div#__layout {
+    height: 100% !important;
   }
 }
 
