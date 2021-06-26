ab13335a0dc9c5f4949b714822de8c610adfe600
phoomin009 <phoomin009@gmail.com>
2021-06-26 13:26:02 +0700

    Add sidebar test

---
+++ test/Sidebar.spec.js
@@ -0,0 +1,10 @@
+@@ -1,9 +0,0 @@
+import { mount } from '@vue/test-utils'
+import Sidebar from '@/components/sidebar.vue'
+
+describe('Sidebar', () => {
+  test('is a Vue instance', () => {
+    const wrapper = mount(Sidebar)
+    expect(wrapper.vm).toBeTruthy()
+  })
+})
