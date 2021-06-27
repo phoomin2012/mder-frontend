40c4209fbe246d42872bad17584c5045cddcfa23
phoomin009 <phoomin009@gmail.com>
2021-06-27 13:14:42 +0700

    Add button translation to sweetalert

--- plugins/sweetalert2.js
+++ plugins/sweetalert2.js
@@ -1,20 +1,14 @@
+/* eslint-disable import/no-mutable-exports */
 /* eslint-disable no-console */
 /* eslint-disable no-unused-vars */
+import Vue from 'vue'
 import Sweetalert from 'sweetalert2'
 
-let i18n = {
-  t (a) { return a }
-}
-
-console.log(global)
-
-export const Swal = Sweetalert.mixin({
-  confirmButtonText: i18n.t('popup.close'),
-  cancelButtonText: i18n.t('popup.cancel'),
-  denyButtonText: i18n.t('popup.deny')
+let _Swal = Sweetalert.mixin()
+export let Swal = Sweetalert.mixin({
 })
 
-export const Toast = Sweetalert.mixin({
+export let Toast = Sweetalert.mixin({
   toast: true,
   position: 'top-end',
   showConfirmButton: false,
@@ -26,13 +20,36 @@ export const Toast = Sweetalert.mixin({
   }
 })
 
-export const Prompt = Sweetalert.mixin({
-  confirmButtonText: i18n.t('popup.confirm'),
+export let Prompt = Sweetalert.mixin({
+//   confirmButtonText: Vue.i18n.t('popup.confirm'),
   showConfirmButton: true,
   showCancelButton: true,
   showLoaderOnConfirm: true
 })
 
-export default function ({ app }) {
-  i18n = app.i18n
+export default function ({ app, i18n }) {
+  _Swal = Sweetalert.mixin({
+    confirmButtonText: app.i18n.t('popup.close'),
+    cancelButtonText: app.i18n.t('popup.cancel'),
+    denyButtonText: app.i18n.t('popup.deny')
+  })
+
+  Swal = _Swal
+  Toast = _Swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.addEventListener('mouseenter', Sweetalert.stopTimer)
+      toast.addEventListener('mouseleave', Sweetalert.resumeTimer)
+    }
+  })
+  Prompt = _Swal.mixin({
+    confirmButtonText: app.i18n.t('popup.confirm'),
+    showConfirmButton: true,
+    showCancelButton: true,
+    showLoaderOnConfirm: true
+  })
 }
