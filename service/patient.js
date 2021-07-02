a1acaad43c277deb2c1b19d4d3bde9e78f2a00ac
phoomin009 <phoomin009@gmail.com>
2021-07-02 23:55:18 +0700

    Add enum data of patient

---
+++ service/patient.js
@@ -0,0 +1,31 @@
+export const PatientStageNumber = {
+  1: 'triage',
+  2: 'investigation',
+  3: 'consultation',
+  4: 'diagnosis',
+  5: 'treatment',
+  60: 'disposition',
+  61: 'discharged',
+  62: 'admitted',
+  63: 'transferred'
+}
+
+export const PatientStageColor = {
+  1: 'secondary',
+  2: 'danger',
+  3: '',
+  4: 'warning',
+  5: 'success',
+  60: 'info',
+  61: 'info',
+  62: 'info',
+  63: 'info'
+}
+
+export const PatientTriageColor = {
+  1: 'blue',
+  2: 'red',
+  3: '#f1cd12',
+  4: '#27AE60',
+  5: 'gray'
+}
