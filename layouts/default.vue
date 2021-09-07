df510b09d1f399c3bf3cbf99bc84b611ca5a6fc1
phoomin2012 <phoomin009@gmail.com>
2021-09-07 13:45:59 +0700

    แก้ไข layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -15,9 +15,12 @@ export default {
 </script>
 
 <style lang="scss">
-
+html, body {
+  width: 100%;
+  height: 100%;
+}
 div#__nuxt {
-  position: absolute;
+  // position: absolute;
   width: 100%;
   height: 100%;
 
